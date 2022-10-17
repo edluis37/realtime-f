58afa758d5af86b83d3de9b3d5eeb2f2216cf990
UmairShah99 <umairshah14@hotmail.com>
2022-10-17 14:10:29 +0100

    css styling improved

--- web/src/guest/view.js
+++ web/src/guest/view.js
@@ -1,35 +1,33 @@
-import { createElement } from "../utils/utils.js"
+import { createElement } from "../utils/utils.js";
 
 export default class GuestView {
- 
+  constructor() {
+    this.online = document.getElementById("online-users");
+    this.offline = document.getElementById("offline-users");
+  }
 
-    constructor() {
-         this.online = document.getElementById("online-users")   
-         this.offline = document.getElementById("offline-users")   
+  display = ({ status, user, newMessage }, handler) => {
+    const guest = createElement("div", "chat-user");
+    guest.textContent = user.nickname ;
+    guest.addEventListener("click", (event) => {
+      handler();
+    });
+    if (status == "online") {
+      this.online.append(guest);
+    } else {
+      this.offline.append(guest);
     }
+    this.guest = guest;
+    if (newMessage) {
+      this.makeGreen();
+    }
+  };
 
-    display = ({status, user, newMessage}, handler) => {
-        const guest = createElement("div", "chat-user")
-        guest.textContent = user.nickname
-        guest.addEventListener("click", (event)=> {
-            handler()
-        })
-        if (status == "online") {
-            this.online.append(guest)
-        }else{
-            this.offline.append(guest)
-        }
-        this.guest = guest
-        if (newMessage) {
-            this.makeGreen()
-        }
-    }
+  makeGreen = () => {
+    this.guest.classList.add("new-message");
+  };
 
-    makeGreen = () => {
-        this.guest.classList.add("new-message")
-    }
-
-    makeGray = () => {
-        this.guest.classList.remove("new-message") 
-    }
+  makeGray = () => {
+    this.guest.classList.remove("new-message");
+  };
 }
